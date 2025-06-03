4a9722db9ddcdbb4b45d8617973aaf4543216177
Aly <122089907+alykapasi@users.noreply.github.com>
2025-06-04 00:50:45 +0530

    some changes to repo structure

---
+++ src/models/cnn1.py
@@ -0,0 +1,11 @@
+# baseline cnn model
+import torch
+from torch import nn
+
+class CNN_v1(nn.Module):
+    """
+    """
+    def __init__(self):
+        pass
+    def forward(self, x):
+        pass
